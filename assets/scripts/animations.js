3b003c8d2a3d7977a51f6d62dffda88e24f1545c
Carlos Daniel <ruuufffaria@gmail.com>
2021-11-21 18:39:20 -0300

    Renaming variables

--- assets/scripts/animations.js
+++ assets/scripts/animations.js
@@ -12,14 +12,15 @@ for (let selector of selectors) {
 }
 
 const checkNodes = () => {
-  const triggerBottom = window.innerWidth > 480 ? window.innerHeight * 70 / 100 : window.innerHeight * 80 / 100
-
-  console.log(triggerBottom)
+  /*
+    Se a largura da tela foi maior que 480px, ele atribui 70% da altura da tela (de cima para baixo). Caso seja menor, ele atribui 80%.
+  */
+  const trigger = window.innerWidth > 480 ? window.innerHeight * 70 / 100 : window.innerHeight * 80 / 100
 
   for (let node of nodes) {
     const elTop = node.getBoundingClientRect().top
 
-    if (elTop < triggerBottom) {
+    if (elTop < trigger) {
       node.classList.add('show')
     } else {
       node.classList.remove('show')
